97898ebb078e7feec793a56dedcb13549d0ea96d
Siarhei Sintsou <Siarhei.Sintsou@burberry.com>
2022-04-30 12:27:07 +0300

    fix configs

--- deployment/standalone/install-env.sh
+++ deployment/standalone/install-env.sh
@@ -1,18 +1,25 @@
 #!/bin/sh
 
+CV_MODELS_SERVING_HOME=/home/cv-recognition-app/cv_models_serving
+WEBAPI_UIPROXY_HOME=/home/cv-recognition-app/webapi_uiproxy
+
 sudo apt-get install nginx
 sudo apt-get install python3-pip
 sudo pip3 install virtualenv
 
+cd $CV_MODELS_SERVING_HOME
 virtualenv -p python3 .env
 source .env/bin/activate
+pip3 install -r requirements.txt
+source .env/bin/deactivate
 
+cd $WEBAPI_UIPROXY_HOME
+virtualenv -p python3 .env
+source .env/bin/activate
 pip3 install -r requirements.txt
 
-
 ##
-# NGinx deployment
+# NGINX deployment
 ##
-sudo cp car_dmg_estimation-demo-webapp /etc/nginx/sites-available/default
 sudo nginx -t
 sudo service nginx restart
